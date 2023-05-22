968b1c8175c16eaf5f2747ee6ec2e790066eec6f
AA9-YG <abewajang@gmail.com>
2023-05-22 14:32:37 -0400

    Added files

---
+++ src/Percolation.java
@@ -0,0 +1,41 @@
+public class Percolation {
+
+    boolean [][] grid;
+    public Percolation(int n) {
+        grid = new boolean[n][n];
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+
+        return false;
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+
+        return false;
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+
+        return 0;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        
+        return false;
+    }
+
+    // test client (optional)
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
