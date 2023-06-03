d442ed362d3a6df62cc52f75bcdce3d3c4d6d3f5
AA9-YG <abewajang@gmail.com>
2023-06-03 14:23:54 -0400

    Implemented isFull method

--- src/Percolation.java
+++ src/Percolation.java
@@ -15,7 +15,7 @@ public class Percolation {
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
-        if (row > 0 || col > 0)
+        if ((row > 0 && col > 0) || (row <= grid.length && col <= grid.length))
             grid[row - 1][col - 1] = true;
         else
             throw new IllegalArgumentException("Grid size must be of at least 1-by-1 ");
@@ -23,22 +23,34 @@ public class Percolation {
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        if (row > 0 && col > 0)
+        if ((row > 0 && col > 0) || (row <= grid.length && col <= grid.length))
             return grid[row - 1][col - 1];
-        else
+
             return false;
     }
 
     // is the site (row, col) full?
-    public boolean isFull(int row, int col) { // Corner cases
-        if (row == 1 && col == 1) {
-            return isOpen(row, col);
+    public boolean isFull(int row, int col) {
+        if (row == 1 && col == 1) { // Top left corner case
+            return isOpen(row, col + 1) || isOpen(row + 1, col);
+        } else if (row == 1 && col == grid.length) { // Top right corner case
+            return isOpen(row, col - 1) || isOpen(row + 1, col);
+        } else if (row == grid.length && col == 1) { // Bottom left corner case
+            return isOpen(row, col + 1) || isOpen(row - 1, col);
+        } else if (row == grid.length && col == grid.length) { // Bottom right corner case
+            return isOpen(row, col - 1) || isOpen(row - 1, col);
+        } else if (row == 1) { // Top row case
+            return isOpen(row, col + 1) || isOpen(row, col - 1) || isOpen(row - 1, col);
+        } else if (row == grid.length) { // Bottom row case
+            return isOpen(row, col + 1) || isOpen(row, col - 1) || isOpen(row + 1, col);
+        } else if (col == 1) { // Left column case
+            return isOpen(row, col + 1) || isOpen(row + 1, col) || isOpen(row - 1, col);
+        } else if (col == grid.length) { // Right column case
+            return isOpen(row, col - 1) || isOpen(row + 1, col) || isOpen(row - 1, col);
+        } else { // All other cases
+            return isOpen(row, col + 1) || isOpen(row + 1, col)
+                    || isOpen(row, col - 1) || isOpen(row - 1, col);
         }
-
-//        if ((row == 1 && col == grid.length) ||
-//                (row == grid.length && col == 1) || (row == grid.length && col == grid.length)))
-
-        return false;
     }
 
     // returns the number of open sites
